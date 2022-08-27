8a9eac11ab4ed0a5b768ed3763f12cf56fb3deb2
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-08-27 12:33:43 -0300

    subindo-pratica-1

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,5 @@
+console.log(Seu_nome = "Thiago Pereira Gonçalves")
+console.log(Seu_email = "thiago19.pgoncalves@gmail.com")
+console.log(Sua_profissão = "Eletricista")
+console.log(Sua_idade = 36)
+console.log(lugar_para_conhecer = "Portugal")
