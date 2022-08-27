b589ef990bcd28bb274452c362fda27ef79f5624
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-08-27 14:52:18 -0300

    subindo-2-pratica-1-template-strings-js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -12,3 +12,7 @@ console.log(suaIdade)
 
 let lugarParaConhecer = "Portugal"
 console.log(lugarParaConhecer)
+
+// Template Strings
+console.log('-')
+console.log(`Olá, ${seuNome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${lugarParaConhecer}. Confirma pra gente alguns dados, A sua idade é ${suaIdade} ,você é ${suaProfissao} e o seu email ${seuEmail}. Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
