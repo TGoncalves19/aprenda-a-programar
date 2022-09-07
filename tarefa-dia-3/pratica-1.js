02c9f0845dc814c0db97e1d66369a09c28d97a15
TGoncalves19 <thiago19.pgoncalves@gmail.com>
2022-09-07 19:01:21 -0300

    subindo-pratica-1-js clear
    

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,17 @@
+//Dominando loops e iteração
+//While de 1 a 9
+let contador = 1
+let n = 9
+
+while(contador <= n ) {
+    console.log (contador)
+    contador = contador + 1
+}
+
+//let i = 1
+//let n = 9
+
+//while(i <= n){
+    //console.log(i)
+    //i++
+//}
